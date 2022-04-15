5ff26d511c86c226ffbef68674e4d3fc85a5493f
Sergey <serega79ser@mail.ru>
2022-04-15 23:40:10 +0600

    Task4 макс из трёх

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+//Максимальное число из трёх
+Console.Clear(); 
+
+WriteLine("Введите первое число");
+int a =  Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число");
+int b =  Convert.ToInt32(ReadLine());
+WriteLine("Введите третье число");
+int c =  Convert.ToInt32(ReadLine());
+
+
+int max = (a > b && a > c)?a:(b > a && b > c)?b:c;
+WriteLine(max);
