07d550c50389a9a744412d042efb829476b4e0d3
Sergey <serega79ser@mail.ru>
2022-04-15 22:29:37 +0600

    Task Удаляет вторую цифру из трёхзначного числа

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/*Задача 2: Напишите программу, которая выводит
+случайное трёхзначное число и удаляет вторую цифру
+этого числа*/
+
+Console.Clear(); 
+
+int num = new Random().Next(100,1000);
+string numS=num.ToString();
+int first = Convert.ToInt32(numS[0].ToString());
+int third = Convert.ToInt32(numS[2].ToString());
+
+/*int a1 = Convert.ToInt32(numS[0].ToString());
+int a2 = Convert.ToInt32(numS[2].ToString());
+Console.WriteLine($"{num}>{a1}{a2}");*/
+Console.WriteLine(num);
+Console.Write(first);
+Console.WriteLine(third);
+
+
