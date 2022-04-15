de8daa0717d43ceaba2041852c2ee1359d5edbac
Sergey <serega79ser@mail.ru>
2022-04-15 22:39:16 +0600

    Task1 показывает наибольшую цифру из двух

--- Ta/Program.cs
+++ Ta/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,32 World!");
+﻿using System;
+
+Console.Clear(); 
+/*2. Напишите программу, которая выводит
+случайное число из отрезка [10, 99] и показывает
+наибольшую цифру числа.*/
+
+
+int num = new Random().Next(10,100);
+string numS=num.ToString();
+int a1= Convert.ToInt32(numS[0].ToString());
+int a2= Convert.ToInt32(numS[1].ToString());
+//int a1 = num/10;
+//int a2 = num%10;
+if(a1 > a2)
+    Console.WriteLine($"{num}->{a1}");
+else
+    Console.WriteLine($"{num}->{a2}");
