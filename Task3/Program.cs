7d84a88b9f79c75eb7e0ceb5698a9adcb4952b03
Sergey <serega79ser@mail.ru>
2022-04-15 23:00:01 +0600

    Task3 Является второе число кратно первому

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Console.Clear(); 
+/*3. Напишите программу, которая будет принимать на
+вход два числа и выводить, является ли второе число
+кратным первому. Если число 2 не кратно числу 1, то
+программа выводит остаток от деление.*/
+WriteLine("Введите первое число");
+int num1 =  Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число");
+int num2 =  Convert.ToInt32(ReadLine());
+if(num1 % num2 == 0)
+    WriteLine("Кратное число");
+else
+    WriteLine("Не кратное, остаток = " + num1 % num2);
