b0fe0113ab103f5ae1b36d1e60fada56eaa01cdc
Sergey <serega79ser@mail.ru>
2022-04-16 15:02:10 +0600

    Task5 Является число квадратом другого числа

---
+++ Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+//Максимальное число из трёх
+Console.Clear(); 
+
+WriteLine("Введите первое число");
+int num1 =  Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число");
+int num2 =  Convert.ToInt32(ReadLine());
+
+/*string result = (num1 * num1 == num2 || num2 * num2 == num1 )?$"Одно из чисел является квадратом другого":
+$"Одно из чисел не является квадратом другого";
+WriteLine(result);*/
+
+string result = (num1 * num1 == num2)?$"{num1} Является квадратом числа {num2}":
+(num2 * num2 == num1)?$"{num2} Является квадратом числа {num2}" : $"Не является квадратом числп";
+WriteLine(result);
