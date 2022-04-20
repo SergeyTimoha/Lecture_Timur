70ce07a4987a15b8019641016c53a3d6ea357aab
Sergey <serega79ser@mail.ru>
2022-04-20 21:25:48 +0600

    Task34 выдаёт таблицу квадратов)

---
+++ Task34_Lec/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine(Math.Pow(i,2));
+}
