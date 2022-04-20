1c2403a07641c5d3b5e3816a825997de635bfaa4
Sergey <serega79ser@mail.ru>
2022-04-20 21:07:36 +0600

    Task33 Зфдача с координатами)

--- Task33_Lec/Program.cs
+++ Task33_Lec/Program.cs
@@ -2,18 +2,16 @@
 
 using System;
 Console.Clear();
-Console.Write("Введите координаты х1: ");
+Console.WriteLine("Введите координаты первой тлчки: ");
 double x1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координаты х2: ");
 double y1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координаты y1: ");
+Console.WriteLine("Введите координаты второй точки: ");
 double x2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координаты y2: ");
 double y2 = Convert.ToDouble(Console.ReadLine());
 
 
-double c = ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-Console.WriteLine($"{Math.Sqrt(c):f2}");
+double c = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine($"{(c):f2}");
 
-double d = Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2);
-Console.WriteLine($"{Math.Sqrt(d):f2}");
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine($"{d:f2}");
