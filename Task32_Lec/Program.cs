62e132ba8acbeed026dcae7fbde434963193e94d
Sergey <serega79ser@mail.ru>
2022-04-20 17:17:54 +0600

    Task32_Lec принимает на вход координаты точки (X и Y)

---
+++ Task32_Lec/Program.cs
@@ -0,0 +1,31 @@
+﻿
+/*Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится 
+эта точка.*/
+
+using System;
+Console.Clear();
+Console.WriteLine("ВВедите координаты х: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите координаты y: ");
+int y = int.Parse(Console.ReadLine());
+if (x> 0&& y>0)
+{
+    Console.WriteLine("Первая четверть");
+}
+if (x<0 && y>0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+if (x<0 && y<0)
+{
+    Console.WriteLine("Третья четверть");
+}
+if (x>0 && y<0)
+{
+    Console.WriteLine("Четвёртая четверть");
+}
+else
+{
+    Console.WriteLine("Введена неправильная четверть");
+}
