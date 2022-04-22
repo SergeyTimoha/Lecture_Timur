32e45d3493ce656dfdb555ddba768dc710ffb270
Sergey <serega79ser@mail.ru>
2022-04-22 09:52:53 +0600

    Task41_Metod Метод String.Join

---
+++ Task41_Menod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.Clear();
+int[] r = new int [5];
+//string t = "123456789ghbd";
+for (int i = 0; i < 5; i++)
+{
+    r[i] = new Random().Next(10);
+
+}
+//Console.WriteLine(String.Join(" , ",t.ToArray()));
+Console.WriteLine(String.Join(" ,",r));
