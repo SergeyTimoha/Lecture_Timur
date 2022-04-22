75e861f5e0d4e23fe151c36ef4475265a737ab3d
Sergey <serega79ser@mail.ru>
2022-04-22 23:51:18 +0600

    Task43 Задача 2

---
+++ Task43_Metod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+Console.Clear();
+Console.WriteLine("Введите число ");
+//int A = int.Parse(Console.ReadLine());
+//int num = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Количество цифр введённого числа : {Count(num)}");
+//Console.WriteLine(Count(int.Parse(Console.ReadLine())));
+
+int Count(int num)
+{
+    int i = 0;
+    while (num > 0)
+    {
+        num/=10;
+        i++;
+    }
+    return i;
+}
+
+
+int Summa(int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++)
+    {
+    
+        sum+= i;
+
+    }
+    return sum;
+}
+
