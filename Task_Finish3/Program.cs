9ca36f6cc22514ef5f8a32f81400ad19ed586ab7
Sergey <serega79ser@mail.ru>
2022-04-26 18:09:48 +0600

    Задача 33: присутсвует ли заданное число

---
+++ Task_Finish3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+
+int[] array=Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray();
+int[] array1=array.Select(x=>x*-1).ToArray();
+//int size=int.Parse(Console.ReadLine());
+//int[] array2=new int[int.Parse(Console.ReadLine())].Select(x=>new Random().Next(0,100)).ToArray();
+//Console.WriteLine(String.Join(" ",array));
+Console.WriteLine(String.Join(" ",array1));
+//Console.WriteLine(String.Join(" ",array2));
